b3c245d53e2fc10c652d5c53d7799240eb199a67
yueding <ydaydl@163.com>
2017-08-07 11:18:08 +0800

    Initial commit

---
+++ app/src/main/java/com/yueding/travelweather/db/District.java
@@ -0,0 +1,51 @@
+package com.yueding.travelweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yueding on 2017/8/7.
+ * SQLite 表District（区/县）
+ */
+
+public class District extends DataSupport {
+
+    private int id;
+
+    private String districtName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDistrictName() {
+        return districtName;
+    }
+
+    public void setDistrictName(String districtName) {
+        this.districtName = districtName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/yueding/travelweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.yueding.travelweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by yueding on 2017/8/7.
+ * SQLite 表Province
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
